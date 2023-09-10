8fd9b40f4ac4094c0217f2f094188a48c51f8bb7
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-09-10 17:51:04 +0300

    Additional implementation on widget
    Proceed on implementing the signal slot mechanism for QComboBox in widget class.

--- widget.cpp
+++ widget.cpp
@@ -2,10 +2,30 @@
 
 Widget::Widget(QWidget *parent){ //: QWidget(parent){
     this->setParent(parent);
-    this->mazeGenerator = new MazeGenerator();
+    this->mazeGenerator = new MazeGenerator(Widget::TRAVERSABLE_MAZE_THRESHOLD, Widget::AUTO_GENERATED_MAZE_COUNT, MazePane::ROW_LENGTH, MazePane::COLUMN_LENGTH, MazePane::START_POS_VALUE, MazePane::TARGET_POS_VALUE, MazePane::EMPTY_GRID_VALUE, MazePane::WALL_GRID_VALUE);
+    this->generateMazes();
+    this->setGenerationSelector();
 }
 
 Widget::~Widget(){
 
 }
 
+void Widget::setGenerationSelector(){
+    this->generationSelector = new QComboBox(this);
+    this->generationSelector->addItem(Widget::SELF_GENERATE_VALUE, QVariant(1));
+    this->generationSelector->addItem(Widget::AUTO_GENERATE_VALUE, QVariant(2));
+
+}
+void Widget::setRadioButtons(){
+
+}
+void Widget::setButtons(){
+
+}
+//Invoke only once during initialization, from constructor
+void Widget::generateMazes(){
+    this->generatedMazes = this->mazeGenerator->generateMazes();
+}
+const string Widget::SELF_GENERATE_VALUE = "Self Generate";
+const string Widget::AUTO_GENERATE_VALUE = "Auto Generate";
--- widget.h
+++ widget.h
@@ -7,17 +7,23 @@
 #include <QButtonGroup>
 #include <QPushButton>
 #include <QHBoxLayout>
+#include <QVariant>
+#include <string>
 
 #include "mazegenerator.h"
 #include "mazepane.h"
+
+using namespace std;
 //main container which will act like a scene
 class Widget : public QWidget{
     Q_OBJECT
 public:
     static const int AUTO_GENERATED_MAZE_COUNT = 50;
     static const int TRAVERSABLE_MAZE_THRESHOLD = 20;
+    static const string SELF_GENERATE_VALUE;
+    static const string AUTO_GENERATE_VALUE;
 private:
-    QComboBox generationSelector; //will be used to select maze generation procedure
+    QComboBox* generationSelector = nullptr; //will be used to select maze generation procedure
     QRadioButton* btAddWall = nullptr;
     QRadioButton* btAddEmpty = nullptr;
     QRadioButton* btSetSource = nullptr;
@@ -32,6 +38,7 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 private:
+    void generateMazes();
     void setGenerationSelector();
     void setRadioButtons();
     void setButtons();
